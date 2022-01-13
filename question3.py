fc63e27723205043a53e8ac40e5b458b54f62ccd
sonikasuggala <96691557+sonikasuggala@users.noreply.github.com>
2022-01-13 22:17:47 +0530

    Create question3.py

---
+++ question3.py
@@ -0,0 +1,6 @@
+skmph = int(input("Enter the speed in kmph: "))
+time = int(input("Enter the time in seconds: "))
+lt = int(input("Enter the length of train :"))
+smps= skmph * (5/18)
+lb = (smps*time)-lt
+print("The lenght of the bridge is:", lb)
